839f9c8d7ad9483e8b14c4f04468f464daa2aaca
IvanSharlovsky <ivansharlovsky@gmail.com>
2023-09-22 22:44:21 +0300

    add few tasks and improve task 4

--- 4.c
+++ 4.c
@@ -2,12 +2,17 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[], char *envp[]) {
+int main(int argc, char *argv[], char *envp[]) {
 
 	printf("num of arg = %d\n", argc);
+
 	for (int i = 0; i < argc; i++) {
-		printf("%d = %s\n", i, argv[i]);
+		printf("argv[%d] = %s\n", i, argv[i]);
 	}
+
+    for (int i = 0; envp[i] != NULL; i++) {
+        printf("envp[%d] = %s\n", i, envp[i]);
+    } 
 
 	return 0;
 	}
