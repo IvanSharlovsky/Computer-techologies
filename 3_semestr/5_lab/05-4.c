bf45751687f5d4f21af4940263e09732d9806e2d
IvanSharlovsky <ivansharlovsky@gmail.com>
2023-12-18 23:14:27 +0300

    add umask(0) and comment

--- 3_semestr/5_lab/05-4.c
+++ 3_semestr/5_lab/05-4.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,6 +13,17 @@ int main()
    size_t  size;
    char    resstring[14];
    char    name[] = "aaa.fifo";
+
+   (void)umask(0);
+
+   /*
+   POSIX.1-2001 says: 
+   "The only portable use of mknod() is to create a FIFO-special file.
+   If mode is not S_IFIFO or dev is not 0, the behavior of mknod() is unspecified."
+
+   However, nowadays one should never use mknod() for this purpose;
+   one should use mkfifo(3), a function especially defined for this purpose.
+   */
 
    if(mknod(name, S_IFIFO | 0666, 0) < 0){
       printf("Can\'t create FIFO\n");
