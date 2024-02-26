71c394011a324b9eaa4956c2d71bba370498e88b
IvanSharlovsky <ivansharlovsky@gmail.com>
2024-02-26 07:17:56 +0300

    add task 3

---
+++ 4_semestr/lab_1/3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int i = 0; i < n; i++)
+            cout << "+";
+
+        cout << endl;
+    }
+        
+}
+
+
+    
