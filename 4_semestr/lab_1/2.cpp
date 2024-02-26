5ff6ef6ee81cce7abe8cd6eb4000206ac647f0c5
IvanSharlovsky <ivansharlovsky@gmail.com>
2024-02-26 07:17:46 +0300

    add task 2

---
+++ 4_semestr/lab_1/2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+
+    cin >> a;
+
+    if (a%13==0) 
+        cout << "Yes" << endl;
+
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
